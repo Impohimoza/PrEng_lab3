c39492922ed2052bc89a0cd5d5017c37f2d2c8ba
malakia3491 <vnss6467@gmail.com>
2026-04-06 20:55:22 +0500

    fix(Users): Add doc strings to functions

--- src/user_repo.py
+++ src/user_repo.py
@@ -1,7 +1,13 @@
 from flask import jsonify
 
 class UsersRepositoryInMemory:
+    """
+    Класс, реализующий паттерн Repository над сущностью User. 
+    Предоставляет функциональность коллекции.
     
+    Attributes:
+        users (list): Коллекция объектов User
+    """
     def __init__(self):
         self.users = [
             {"id": 1, "name": "Alice", "age": 25},
@@ -12,16 +18,38 @@ class UsersRepositoryInMemory:
         return len(self.users)
 
     def get_all(self):
+        """
+        Функция возвращает json всех пользователей
+        
+        :return: json объект
+        """            
         return jsonify(self.users)
 
     def add(self, new_user):
+        """
+        Функция добавляет новый объект пользователя в коллекцию
+        
+        :param new_user: объект нового пользовотеля
+        """           
         self.users.append(new_user)
 
     def get(self, user_id):
+        """
+        Функция возвращает объект пользователя по айди
+        
+        :param user_id: айди пользователя
+        :return: объект пользователя
+        """                
         user = next((u for u in self.users if u["id"] == user_id), None)
         return user
 
     def update(self, user_id, data):
+        """
+        Функция обновляет атрибуты пользователя
+        
+        :param user_id: айди пользователя
+        :return: user обновлённый объект пользователя     
+        """           
         user = next((u for u in self.users if u["id"] == user_id), None)
         if not user:
             raise ValueError("User not found")
@@ -31,6 +59,11 @@ class UsersRepositoryInMemory:
         return user
 
     def delete(self, user_id):
+        """
+        Функция удаляет пользователя по айди
+        
+        :param user_id: айди пользователя  
+        """            
         user = next((u for u in self.users if u["id"] == user_id), None)
         if not user:
             raise ValueError("User not found")            
